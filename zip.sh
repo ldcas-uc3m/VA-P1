3c98d3d5dcfbd9407f7f5fb948a14cd40a85a9a5
Luis Daniel Casais Mezquida <78721925+ldcas-uc3m@users.noreply.github.com>
2024-03-22 15:13:42 +0100

    Initial commit

---
+++ zip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Bash script to zip the whole project in order to make it deriverable
+# please make sure zip and texlive are installed
+
+OUTFILE=../outfile.zip
+[ -e $OUTFILE ] && rm $OUTFILE  # remove if exists already
+
+# compile the report (and save it to root folder)
+echo "Compiling the report..."
+
+latexmk -cd -shell-escape -pdf report/report.tex 
+
+cp report/report.pdf .
+
+
+cd src
+
+# <clean stuff up>
+
+cd ..
+
+# zip it (excluding useless stuff)
+echo "Zipping..."
+zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* README.md
+
+# cleanup
+echo "Cleaning up..."
+rm report.pdf
